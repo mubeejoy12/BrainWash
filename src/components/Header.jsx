1a31c0a7b47f7eee0f63744052052f8f87776bbc
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-26 08:13:15 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 z-50 bg-n-8/90 background-blur-sm border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+
+        <div className="flex items-center px-5 lg:px-7.5">
+
+        </div>
+        Header
+        
+        </div>
+  );
+};
+
+export default Header;
