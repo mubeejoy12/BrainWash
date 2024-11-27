981c743d546ac2511fe63769f136886d1678f002
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-27 23:26:03 +0100

    Imported a component ButtonGradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import ButtonGradient from "../assets/svg/ButtonGradient";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/Header";
 function App() {
