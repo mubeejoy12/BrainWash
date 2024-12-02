41a35a9052095c514166d0e1ee0a05a860199ae1
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-12-02 11:24:40 +0100

    import core components and images

---
+++ src/components/RoadMap.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Section from "../design/Section";
+import Heading from "../design/Heading";
+import { roadmap } from "../constants";
+import TagLine from "./TagLine";
+import { check2, Grid, loading1 } from "../assets";
+import { Gradient } from "../design/Services";
+import Button from "./Button";
