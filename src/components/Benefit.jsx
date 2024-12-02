29d3973171d96707485f8322681367427b370ff8
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-12-02 11:33:22 +0100

    imported core coomponents

---
+++ src/components/Benefit.jsx
@@ -0,0 +1,6 @@
+import { benefits } from "../constants";
+import Heading from "../design/Heading";
+import Section from "../design/Section";
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "../design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
