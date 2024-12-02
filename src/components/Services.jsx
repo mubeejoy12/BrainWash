f30f433b3e3fedb6fd8fadc91a1012af6bf234c3
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-12-02 11:20:32 +0100

    map over the icons for easier coding

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,3 +10,17 @@ import {
   VideoBar,
   VideoChatMessage,
 } from "../design/Services";
+                  {brainwaveServicesIcons.map((item, index) => (
+                    <li
+                      key={index}
+                      className={`rounded-2xl flex items-center justify-center ${
+                        index === 2
+                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]"
+                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                      }`}
+                    >
+                      <div
+                        className={
+                          index === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
