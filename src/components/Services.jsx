d01fde34fb81ee7bd9fa70ecef929ecd7de4604f
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-12-02 11:19:53 +0100

    imported core components

---
+++ src/components/Services.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Section from "../design/Section";
+import Heading from "../design/Heading";
+import { service1, service2, service3, check } from "../assets";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import Generating from "./Generating";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "../design/Services";
