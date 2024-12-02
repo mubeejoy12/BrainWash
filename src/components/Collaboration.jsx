0ded94db70668ec7039834887247915317d8c6d3
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-12-02 11:32:26 +0100

    imported code components

---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,6 @@
+import { brainwaveSymbol, check } from "../assets";
+import { collabApps, collabContent, collabText } from "../constants";
+import Section from "../design/Section";
+import Button from "./Button";
+import { RightCurve,LeftCurve } from "../design/Collaboration";
+
